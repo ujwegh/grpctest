7e6d644ad5b091df8eee71529233b7fd321f0ead
Nikita Aleksandrov <nikita.aleksandrov@abfprogramming.com>
2022-12-28 13:59:21 +0400

    init

---
+++ src/test/java/com/example/grpcservice1/Grpcservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grpcservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
